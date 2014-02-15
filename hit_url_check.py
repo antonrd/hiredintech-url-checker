6a3f051f26fa2e4cdcd71dd84d978580754eac61
Anton Dimitrov <dimitrov.anton@gmail.com>
2014-02-15 09:59:13 +0200

    Improving the code a bit, removing email.

--- hit_url_check.py
+++ hit_url_check.py
@@ -6,22 +6,21 @@ import urllib
 from email.mime.text import MIMEText
 from subprocess import Popen, PIPE
 
-FROM_EMAIL = "hiredintech@gmail.com"
-TO_EMAIL = "hiredintech@gmail.com"
+FROM_EMAIL = "<email>"
+TO_EMAIL = "<email>"
 
 err_cnt = 0
 
-urls_file = open("hit_urls.txt", "r")
-for line in urls_file:
-  url = line.strip()
-  if len(url) == 0:
-    continue
-  code = urllib.urlopen(url).getcode()
-  print '%d --- %s' % (code, url)
-  if code != 200:
-    print >> sys.stderr, "Received code %d from url %s" % (code, url)
-    err_cnt += 1
-urls_file.close
+with open("hit_urls.txt", "r") as urls_file:
+  for line in urls_file:
+    url = line.strip()
+    if len(url) == 0:
+      continue
+    code = urllib.urlopen(url).getcode()
+    print '%d --- %s' % (code, url)
+    if code != 200:
+      print >> sys.stderr, "Received code %d from url %s" % (code, url)
+      err_cnt += 1
 
 if err_cnt == 0:
   print "Finished with no errors"
@@ -30,6 +29,6 @@ else:
   msg = MIMEText("There were %d problematic URLs" % err_cnt)
   msg["From"] = FROM_EMAIL
   msg["To"] = TO_EMAIL
-  msg["Subject"] = "Problemnatic URLs found in HiredInTech"
+  msg["Subject"] = "Problematic URLs found in HiredInTech"
   p = Popen(["/usr/sbin/sendmail", "-t"], stdin=PIPE)
   p.communicate(msg.as_string())
